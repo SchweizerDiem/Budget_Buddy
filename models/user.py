49195d2dde79220619f7a51524f6596f677b2c48
SchweizerDiem <gabrielrochaxtz@gmail.com>
2025-04-08 18:08:27 +0100

    new user base

--- models/user.py
+++ models/user.py
@@ -1,32 +1,71 @@
-from enum import Enum
+from datetime import datetime
+import uuid
+
+class user:
+    def __init__ (self):
+        self.id = str(uuid.uuid4())  # Generate unique ID for transaction
+        self.category = []
+        self.transactions = []
 
 
-class Role(Enum):
-    MANAGEMENT = "management"
-    SALES = "sales"
+    # NOTE: TRANSACTIONS
+    def add_category(self, name_of_category: str):
+        self.category.append(name_of_category)
 
+    def add_transaction(self, amount, tipo, category, date=datetime.now().strftime("%Y-%m-%d"), description=None):
+        if category in self.category:
+            self.transactions.append([amount, description, tipo, category, date])
 
-class User:
-    def __init__(self, user_id, name, role, store_id):
-        self.user_id = user_id
-        self.name = name
-        self.role = role
-        self.store_id = store_id
+    # NOTE: DEL
+    def del_transaction(self):
+        for i, tra in enumerate(self.transactions):
+            print(f"{i} {tra}")
+        self.transactions.pop(int(input("Type the index: ")))
 
-    def __str__(self):
-        return f"User(ID: {self.user_id}, Name: {self.name}, Role: {self.role.value}, Store ID: {self.store_id})"
+    # NOTE: EDIT
+    def edit_transaction(self):
+        for i, tra in enumerate(self.transactions):
+            print(f"{i} {tra}")
+        trans = int(input("Select transaction: "))
+        while True:
+            print("1. Amount")
+            print("2. Description")
+            print("3. Type")
+            print("4. Category")
+            print("5. Date")
+            option = int(input("Select field: "))
 
-    def is_management(self):
-        return self.role == Role.MANAGEMENT
+            match option:
+                case 1:
+                    self.transactions[trans][0] = int(input("New Amount: "))
+                    print("Edited ", self.transactions[trans][0])
+                    print(self.transactions[trans])
+                case 1:
+                    self.transactions[trans][1] = input("New Description: ")
+                    print("Edited ", self.transactions[trans][1])
+                    print(self.transactions[trans])
+                case 1:
+                    self.transactions[trans][2] = input("New type: ")
+                    print("Edited ", self.transactions[trans][2])
+                    print(self.transactions[trans])
+                case 1:
+                    self.transactions[trans][3] = input("New Category: ")
+                    print("Edited ", self.transactions[trans][3])
+                    print(self.transactions[trans])
+                case 1:
+                    self.transactions[trans][4] = input("New Date (%Y-%m-%d): ")
+                    print("Edited ", self.transactions[trans][4])
+                    print(self.transactions[trans])
+                case default:
+                    print("Not a valid option!!")
 
-    def is_sales(self):
-        return self.role == Role.SALES
+    def __str__(self) -> str:
+        return f"{self.id}\n{self.transactions}"
 
-    def has_permission(self, action):
-
-        # Dictionary which holds a list of permissions for each role
-        permissions = {
-            Role.MANAGEMENT: ["view_reports", "manage_users", "approve_transactions"],
-            Role.SALES: ["record_sales", "view_inventory"]
-        }
-        return action in permissions[self.role]
+usr = user()
+usr.add_category("Vendas")
+usr.add_category("Compra")
+usr.add_transaction(130, "expense", "Vendas")
+usr.add_transaction(142, "income", "Compra")
+print("######")
+usr.edit_transaction()
