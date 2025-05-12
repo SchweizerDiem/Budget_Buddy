5fb5545a09cbfa153346ce0f8628a57fe23208da
SchweizerDiem <gabrielrochaxtz@gmail.com>
2025-05-12 11:53:37 +0100

    adding images and dashboard page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+// helper functions
+import { fetchData } from "../helpers";
+
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+}
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard;
